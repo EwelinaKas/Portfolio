cfa47fd70b29c2893601484b15554e17c90c6727
eeewelix <eeewelix@gmail.com>
2023-07-03 00:00:25 +0200

    Add portraits.html

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    # PHOTOS
+    path('portraits/', views.portraits_view, name='portraits'),
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 def home(request):
     return render(request,
                   'main/home.html')
+
+
+def portraits_view(request):
+    return render(request,
+                  'main/portraits.html')
+
